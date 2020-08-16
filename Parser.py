ad7d695f5a32413d0ebbb9a108c3c251a1e0da1a
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2020-08-15 20:27:12 -0400

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -3,18 +3,14 @@ import requests
 with open("rawHTML.txt", "r", encoding="utf-8") as File1:
     file_contents = File1.read().split(sep="\n")
 data = []
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'  # Get around rate limit
 headers = {'User-Agent': user_agent}
-# poke_id = "/wiki/Bulbasaur"
-# wiki_link = f'https://pixelmonmod.com{poke_id}'
-# source = requests.get(wiki_link, headers=headers).text
 skipped = []
 for thing in file_contents:
     poke_id = thing.split('"')[1]
     poke_name = thing.split(">")[1]
     wiki_link = f'https://pixelmonmod.com{poke_id}'
     source = requests.get(wiki_link, headers=headers).text
-    # print(wiki_link)
     soup = BeautifulSoup(source, "lxml")
     for table in soup.find_all('span'):
         if "Ultra Rare" in table:
